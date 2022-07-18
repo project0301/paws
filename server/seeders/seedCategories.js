0405f97f47cc702cf2a75d4ab0cb27932f4fe9c6
Royden Semedo <roydensemedo@utmuser147-170.wireless.utoronto.ca>
2022-07-18 12:20:26 -0400

    added initial seed files

---
+++ server/seeders/seedCategories.js
@@ -0,0 +1,3 @@
+const Category = require("../models/Category");
+const mongoose = require("mongoose");
+const connection = require("../config/connection");
---
+++ server/seeders/seedProducts.js
@@ -0,0 +1,3 @@
+const Product = require("../models/Product");
+const mongoose = require("mongoose");
+const connection = require("../config/connection");
