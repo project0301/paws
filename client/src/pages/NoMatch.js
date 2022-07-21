2b8e2ae54b792a497d15ff27ff1fe5202a315ae7
Swapna <swapnamenon8@gmail.com>
2022-07-21 16:02:26 -0400

    added import from cart component

---
+++ client/src/pages/NoMatch.js
@@ -0,0 +1,14 @@
+import React from "react";
+
+
+const NoMatch = () => {
+  return (
+    <div>
+      <Jumbotron>
+        <h1>404 Error Page Not Found</h1>
+      </Jumbotron>
+    </div>
+  );
+};
+
+export default NoMatch;
