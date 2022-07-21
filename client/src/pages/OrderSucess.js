ab44277a5765ace91a27515215baef5f84044b10
ianaack <ianaack@gmail.com>
2022-07-21 19:05:38 -0400

    Fixed page files spelling errors and formatting

--- client/src/pages/OrderSucess.js
+++ client/src/pages/OrderSucess.js
@@ -1,41 +1,42 @@
-import React, {useEffect} from 'React';
-import {useMutations} from '@apollo/client';
-import {ADD_ORDER} from '../utils/mutations';
-import { idbPromise } from '../utils/helpers';
+import React, { useEffect } from "React";
+import { useMutation } from "@apollo/client";
+import Jumbotron from "../components/Jumbotron";
+import { ADD_ORDER } from "../utils/mutations";
+import { idbPromise } from "../utils/helpers";
 
-function sucess (){
-    const [addOrder] = useMutation(ADD_ORDER);
+function Success() {
+	const [addOrder] = useMutation(ADD_ORDER);
+
+	useEffect(() => {
+		async function saveOrder() {
+			const cart = await idbPromise("cart", "get");
+			const products = cart.map((item) => item._id);
+
+			if (products.length) {
+				const { data } = await addOrder({ variables: { products } });
+				const productData = data.addOrder.products;
+
+				productData.forEach((item) => {
+					idbPromise("cart", "delete", item);
+				});
+			}
+
+			setTimeout(() => {
+				window.location.assign("/");
+			}, 3000);
+		}
+
+		saveOrder();
+	}, [addOrder]);
+
+	return (
+		<div>
+			<Jumbotron>
+				<h1>Order Placed</h1>
+				<h2>Redirected to the home page</h2>
+			</Jumbotron>
+		</div>
+	);
 }
-useEffect(() => {
-    async function saveOrder() {
-      const cart = await idbPromise('cart', 'get');
-      const products = cart.map((item) => item._id);
 
-      if (products.length) {
-        const { data } = await addOrder({ variables: { products } });
-        const productData = data.addOrder.products;
-
-        productData.forEach((item) => {
-          idbPromise('cart', 'delete', item);
-        });
-      }
-
-      setTimeout(() => {
-        window.location.assign('/');
-      }, 3000);
-    }
-
-    saveOrder();
-  }, [Add_Order]);
-
- return (
-    <div>
-        <Jumbotron>
-            <h1>Order Placed</h1>
-            <h2>Redirected to the Main page </h2>
-        </Jumbotron>
-    </div>
-  );
-
-  export default OrderScucess;
-
+export default Success;
