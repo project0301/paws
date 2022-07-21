c07643d2ff7a4dd542be9b8b14a0c17d5065eb83
Swapna <swapnamenon8@gmail.com>
2022-07-20 22:30:59 -0400

    created the order history page

---
+++ client/src/pages/orderhistory.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+
+import { useQuery } from '@apollo/client';
+import { QUERY_USER } from '../utils/queries';
+
+function OrderHistory() {
+  const { data } = useQuery(QUERY_USER);
+  let user;
+
+  if (data) {
+    user = data.user;
+}
