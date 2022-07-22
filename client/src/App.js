c8efd6051509fa2ef84d2f6b019c5993160ddae4
ianaack <ianaack@gmail.com>
2022-07-21 20:51:55 -0400

    Included stripe-js/react

--- client/src/App.js
+++ client/src/App.js
@@ -8,12 +8,15 @@ import {
 } from "@apollo/client";
 import { setContext } from "@apollo/client/link/context";
 
+import { loadStripe } from "@stripe/stripe-js";
+import { Elements } from "@stripe/react-stripe-js";
+
 import Home from "./pages/Home";
 import Detail from "./pages/ItemDetail";
 import NoMatch from "./pages/NoMatch";
 import Login from "./pages/Signin";
 import Signup from "./pages/Signup";
-import Nav from "./components/Nav";
+// import Nav from "./components/Nav";
 import { StoreProvider } from "./utils/GlobalState";
 import Success from "./pages/OrderSuccess";
 import OrderHistory from "./pages/OrderHistory";
@@ -37,25 +40,29 @@ const client = new ApolloClient({
 	cache: new InMemoryCache(),
 });
 
+const stripePromise = loadStripe("pk_test_TYooMQauvdEDq54NiTphI7jx");
+
 function App() {
 	return (
 		<ApolloProvider client={client}>
-			<Router>
-				<div>
-					<StoreProvider>
-						<Nav />
-						<Routes>
-							<Route path="/" element={<Home />} />
-							<Route path="/login" element={<Login />} />
-							<Route path="/signup" element={<Signup />} />
-							<Route path="/success" element={<Success />} />
-							<Route path="/orderHistory" element={<OrderHistory />} />
-							<Route path="/products/:id" element={<Detail />} />
-							<Route element={<NoMatch />} />
-						</Routes>
-					</StoreProvider>
-				</div>
-			</Router>
+			<Elements stripe={stripePromise}>
+				<Router>
+					<div>
+						<StoreProvider>
+							{/* <Nav /> */}
+							<Routes>
+								<Route path="/" element={<Home />} />
+								<Route path="/login" element={<Login />} />
+								<Route path="/signup" element={<Signup />} />
+								<Route path="/success" element={<Success />} />
+								<Route path="/orderHistory" element={<OrderHistory />} />
+								<Route path="/products/:id" element={<Detail />} />
+								<Route element={<NoMatch />} />
+							</Routes>
+						</StoreProvider>
+					</div>
+				</Router>
+			</Elements>
 		</ApolloProvider>
 	);
 }
