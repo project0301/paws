912d189363f0e63fa2421db09417f926cad3ed9d
Feroz Zargar <Ferozzargar@hotmail.com>
2022-07-22 11:17:46 -0400

    index.js added to components/ProductList

---
+++ client/src/components/ProductList/index.js
@@ -0,0 +1,66 @@
+import React, { useEffect } from "react";
+import ProductItem from "../ProductItem";
+import { useStoreContext } from "../../utils/GlobalState";
+import { UPDATE_PRODUCTS } from "../../utils/actions";
+import { useQuery } from "@apollo/client";
+import { QUERY_PRODCUTS, QUERY_PRODUCTS } from "../../utils/queries";
+import { idbPromise } from "../../utils/helpers";
+import spinner from "../../assets/spinner.gif";
+
+function ProductList() {
+    const [state, dispatch] = useStoreContext();
+
+    const { currentCategory } = state;
+
+    const { loading, data } = useQuery(QUERY_PRODUCTS);
+
+    useEffect(() => {
+        if (data) {
+            dispatch({
+                type: UPDATE_PRODUCTS, 
+                products: data.products, 
+            });
+            data.products.forEach((product) => {
+                idPromise("products", "get").then((products) => {
+                    dispatch({
+                        type: UPDATE_PRODUCTS,
+                        products: products,
+                    })
+                })
+            }
+        }, [data, loading, dispatch]);
+
+        function filterProducts() {
+            if (!currentCategory) {
+                return state.products;
+            }
+
+            return state.products.filter(
+                (product) => product.category._id === currentCategory
+            );
+        }
+        return (
+            <div className="my-2">
+            <h2>Our Products:</h2>
+            {state.products.length ? (
+                <div className="flex-row">
+                {filterProducts().map((product) => (
+                <ProductItem
+                    key={product.id}
+                    _id={product._id}
+                    image={product.image}
+                    name={product.name}
+                    price={product.price}
+                />
+                ))}
+                </div>
+            ) : (
+                <h3>You haven't added any items yet!</h3>
+            )}
+            {loading ? <img src={spinner} alt="loading" /> : null}
+            </div>
+        );
+                
+        }
+
+        export default ProductList;
