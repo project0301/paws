aebffd0929b078ca476bfb3c6c9589f953f43a09
Swapna <swapnamenon8@gmail.com>
2022-07-22 14:10:39 -0400

    added filter data for the searchbar

--- client/src/pages/SearchBar.js
+++ client/src/pages/SearchBar.js
@@ -4,14 +4,25 @@ import { IconButton } from '@chakra-ui/react'
 <IconButton  colorScheme='blue' aria-label='Search database' icon={<searchIcon/>} />
 
 function SearchBar({placeholder, data}) {
+    const[filteredData, setFilteredData] = useState[()];
+
     return (
         <div className ="search">
             <div className ="searchInputs">
                 <input type="text" placeholder= {placeholder} />
                 <div className="searchIcon"><searchIcon></searchIcon>
                 </div>
+                {filteredData
                 <div className="dataresult">
+                    {data.map((value, key)=>{
+                        return (
+                            <a className="dataItem" href={value.category}>
+                                <p>{value.category}</p>
+                            </a>
+                        )
+                    })}
                 </div>
+                }
             </div>
         </div>
     );
