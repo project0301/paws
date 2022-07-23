0297b8ea27d8c8ef6bbfce46e25ef94af2319969
ianaack <ianaack@gmail.com>
2022-07-23 13:28:36 -0400

    Removed ChakraProvider duplicate

--- client/src/index.js
+++ client/src/index.js
@@ -3,7 +3,6 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import { ChakraProvider } from '@chakra-ui/react';
-import { ChakraProvider } from '@chakra-ui/icons';
 import customTheme from './components/extendTheme';
 
 
