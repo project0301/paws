11812e63ff73ac1b6ea8e136a8f3129c1265ee49
Swapna <swapnamenon8@gmail.com>
2022-07-22 22:12:56 -0400

    correction of useState () error

--- client/src/components/SearchBar/index.js
+++ client/src/components/SearchBar/index.js
@@ -4,11 +4,11 @@ import { IconButton } from '@chakra-ui/react'
 <IconButton colorScheme='blue' aria-label='Search database' icon={<searchIcon/>} />
 
 function SearchBar({placeholder, data}) {
-    const[filteredData, setFilteredData] = useState[()];
+    const[filteredData, setFilteredData] = useState({});
     const handleFilter = (event) => {
        const searchKeyword = event.target.value
        const newfilter = data.filter((value)=> {
-          return name.category.toLowerCase().includes(searchKeyword.toLowerCase());
+          return value.category.toLowerCase().includes(searchKeyword.toLowerCase());
        });
        setFilteredData(newfilter);
     };
@@ -21,9 +21,9 @@ function SearchBar({placeholder, data}) {
                 <div className="dataresult">
                     {data.map((value,key) => {
                         return(
-                            <a className="dataItem" href={name.category}>
+                            <a className="dataItem" href={value.category}>
                                 <p>
-                                    {name.category}
+                                    {value.category}
                                 </p>
                             </a>
                         )
