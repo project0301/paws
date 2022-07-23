581cbab68ac1fbee583ecafe5e406f9e463d27fe
Schris <tovechris48@gmail.com>
2022-07-22 20:24:52 -0400

    Banner-jsx-fix

--- client/src/components/Banner.jsx
+++ client/src/components/Banner.jsx
@@ -7,8 +7,8 @@ function Banner() {
         <Box>
           <Image boxSize="90px" src="client/src/assets/logo.png" alt="paws logo" />
         </Box>
-        <Box d="flex" align-items="right" justifyContent="space-between" ml={6} mr={6}>
-          <Button type="button" class="login btn" label="login button" pr={3} colorScheme="gray.600" font-size="sm" variant="link">
+        <Box>
+          <Button pr={3} colorScheme="gray.600" font-size="sm" variant="link">
             <a href="[placeholder-for-login-page-location]" alt="login to your paws account" target="_blank" rel="noopener noreferrer"></a>
             Login
         </Button>
