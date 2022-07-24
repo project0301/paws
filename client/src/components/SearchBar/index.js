764387cfcce30c7821e4fa43b375dd9637772b45
Swapna <swapnamenon8@gmail.com>
2022-07-24 15:18:14 -0400

    modified searchbar.js

--- client/src/components/SearchBar/index.js
+++ client/src/components/SearchBar/index.js
@@ -1,43 +1,5 @@
 <<<<<<< HEAD
-import React from "react"; 
-import { IconButton } from '@chakra-ui/react'
 
-<IconButton colorScheme='blue' aria-label='Search database' icon={<searchIcon/>} />
-
-function SearchBar({placeholder, data}) {
-    const[filteredData, setFilteredData] = useState({});
-    const handleFilter = (event) => {
-       const searchKeyword = event.target.value
-       const newfilter = data.filter((value)=> {
-          return value.category.toLowerCase().includes(searchKeyword.toLowerCase());
-       });
-       setFilteredData(newfilter);
-    };
-    return (
-        <div className="search">
-            <div className="searchInputs">
-                <input type="text" placeholder={placeholder} onChange={handleFilter}/>
-                <div className="searchIcon"><searchIcon></searchIcon>
-                </div>
-                <div className="dataresult">
-                    {data.map((value,key) => {
-                        return(
-                            <a className="dataItem" href={value.category}>
-                                <p>
-                                    {value.category}
-                                </p>
-                            </a>
-                        )
-                    }
-                    )}
-                </div>
-                
-                
-            </div> 
-
-        </div>
-    );
-                }
 
     
 =======
