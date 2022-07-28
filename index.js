1f19dcbb4bf16f7de89bf2add3e2c43325284c80
project0301 <109397767+project0301@users.noreply.github.com>
2022-07-27 20:39:26 -0400

    Add files via upload
    Search Bar index.js

---
+++ index.js
@@ -0,0 +1,66 @@
+import React, { useState } from "react";
+import { Input, InputGroup, InputRightElement, Box } from "@chakra-ui/react";
+import { SearchIcon } from "@chakra-ui/icons";
+import { QUERY_GET_PRODUCTS } from "../../utils/queries";
+import { useQuery } from "@apollo/client";
+import { useStoreContext } from "../../utils/GlobalState";
+import { UPDATE_PRODUCTS } from "../../utils/actions";
+import spinner from "../../assets/spinner.gif";
+
+function SearchBar({ placeholder, data }) {
+	const [keyWord, setKeyWord] = useState("");
+	const [dispatch] = useStoreContext();
+
+	const { loading, data: productData } = useQuery(QUERY_GET_PRODUCTS, {
+		variables: {
+			search: keyWord,
+		},
+	});
+
+	const [setFilteredData] = useState({});
+
+	const handleFilter = (event) => {
+		event.preventDefault();
+
+		const searchKeyword = event.target.value;
+		setKeyWord(searchKeyword);
+
+		setFilteredData(searchKeyword);
+		console.log(productData);
+
+		dispatch({
+			type: UPDATE_PRODUCTS,
+			products: productData.getProducts.products,
+		});
+
+		setFilteredData(searchKeyword);
+	};
+
+	return (
+		<Box>
+			<InputGroup>
+				<Input
+					type="text"
+					htmlSize={"100%"}
+					width={"auto"}
+					placeholder="Search for a product"
+					onChange={handleFilter}
+				/>
+				<InputRightElement>
+					<SearchIcon />
+				</InputRightElement>
+			</InputGroup>
+			{loading ? <img src={spinner} alt="loading" /> : null}
+		</Box>
+	);
+}
+
+export default SearchBar;
+
+
+
+
+
+
+
+
